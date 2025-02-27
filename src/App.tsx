fb562dee2fd556f8f8a82b50428bd763b8f18004
nabetse <esteban.diezlaiz@gmail.com>
2025-02-27 18:27:23 +0100

    custom id project

--- src/App.tsx
+++ src/App.tsx
@@ -7,7 +7,7 @@ import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
 import { ActionButtonList } from './components/ActionButtonList'
 import { SmartContractActionButtonList } from './components/SmartContractActionButtonList'
 import { InfoList } from './components/InfoList'
-import { projectId, metadata, networks, wagmiAdapter } from './config'
+import { projectId, metadata, networks, wagmiAdapter, customId } from './config'
 
 import "./App.css"
 
@@ -53,16 +53,18 @@ export function App() {
       <h1>AppKit Wagmi React dApp Example</h1>
       <WagmiProvider config={wagmiAdapter.wagmiConfig}>
         <QueryClientProvider client={queryClient}>
-            <appkit-button />
-            <ActionButtonList sendHash={receiveHash} sendSignMsg={receiveSignedMsg} sendBalance={receivebalance}/>
-            <SmartContractActionButtonList />
-            <div className="advice">
-              <p>
-                This projectId only works on localhost. <br/>
-                Go to <a href="https://cloud.reown.com" target="_blank" className="link-button" rel="Reown Cloud">Reown Cloud</a> to get your own.
-              </p>
-            </div>
-            <InfoList hash={transactionHash} signedMsg={signedMsg} balance={balance}/>
+          <appkit-button />
+          <ActionButtonList sendHash={receiveHash} sendSignMsg={receiveSignedMsg} sendBalance={receivebalance} />
+          <SmartContractActionButtonList />
+          <div className="advice">
+            {!customId ? <p>
+              This projectId only works on localhost.<br />
+              Go to <a href="https://cloud.reown.com" target="_blank" className="link-button" rel="Reown Cloud">Reown Cloud</a> to get your own.
+            </p>
+              : <p>Running your own project id </p>
+            }
+          </div>
+          <InfoList hash={transactionHash} signedMsg={signedMsg} balance={balance} />
         </QueryClientProvider>
       </WagmiProvider>
     </div>
