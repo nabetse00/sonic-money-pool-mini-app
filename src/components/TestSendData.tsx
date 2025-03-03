6aeb69a1befa86fa397e878ffadd8bb5bab7b215
nabetse <esteban.diezlaiz@gmail.com>
2025-03-03 17:50:17 +0100

    test custom method

--- src/components/TestSendData.tsx
+++ src/components/TestSendData.tsx
@@ -1,12 +1,16 @@
 import { postEvent } from '@telegram-apps/bridge';
+import {v4 as uuidv4} from 'uuid';
 
 export function TestSendDataButton() {
 
     async function handleClick() {
-        const obj = { name: "me", value: "test" }
-        const value = JSON.stringify(obj)
-        console.log(`test post event ${value}`)
-        postEvent('web_app_data_send', { data: value })
+        // const obj = { name: "me", value: "test" }
+        // const value = JSON.stringify(obj)
+        // console.log(`test post event ${value}`)
+        // postEvent('web_app_data_send', { data: value })
+        const uuid = uuidv4()
+        console.log(`test post eventcsutom method  ${uuid}`)
+        postEvent('web_app_invoke_custom_method', {req_id:uuid, method:"test", params:"some params" })
     }
 
     return <>
